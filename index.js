1138c104adece3ff13f60aa03f0502c43fb22b32
srparamu <srparamu0716@gmail.com>
2020-01-25 13:31:39 -0500

    added some responsiveness and hidden navbar when scrolling down (done on desktop)

---
+++ index.js
@@ -0,0 +1,18 @@
+//Adjusts navbar being hidden when scrolling down and visible when scrolling up
+//Main Source: W3schools
+var prevScrollpos = window.pageYOffset;
+
+window.onload = () => {
+    var prevScrollpos = window.pageYOffset;
+    window.onscroll = function () {
+        var currentScrollPos = window.pageYOffset;
+        console.log(prevScrollpos, currentScrollPos);
+        let navbar = document.getElementById("navbar");
+        if (prevScrollpos > currentScrollPos || prevScrollpos == 0){
+            navbar.style.top = "92%";
+        } else {
+            navbar.style.top = "100%";
+        }
+        prevScrollpos = currentScrollPos;
+    }
+}
