be90277dda14316394d00ebb668287216f8b91cf
srparamu <srparamu0716@gmail.com>
2020-01-28 21:38:46 -0500

    adjusted navbar and added jquery to smooth scroll (done on desktop)

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 //Adjusts navbar being hidden when scrolling down and visible when scrolling up
 //Main Source: W3schools
-var prevScrollpos = window.pageYOffset;
 
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "0";
-    } else {
+window.onload = () => {  
+
+//Smooth Scrolling JQuery
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
         document.getElementById("navbar").style.top = "-110px";
+        document.getElementById("myself").style.marginTop = "0";
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+//In Case users want to return the navbar without scrolling up
+document.addEventListener('keydown', logKey);
+    function logKey(e){
+        document.getElementById("navbar").style.top = "0";
+        document.getElementById("myself").style.marginTop = null;
     }
-    prevScrollpos = currentScrollPos;
+
 }
